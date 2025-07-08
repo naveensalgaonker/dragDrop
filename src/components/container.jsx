ae11abd3c05c6adfb218b7e196bb61e1d126404b
naveensalgaonker <naveensalgaonker@gmail.com>
2025-07-08 16:01:46 +0530

    feat: basic drag drop within cells

--- src/components/container.jsx
+++ src/components/container.jsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react'
 const Container = () => {
   const BOARD_SIZE = [4, 4]
   const [board, setBoard] = useState()
+  const [dragging, setDragging] = useState();
 
   useEffect(() => {
     generateBoard()
@@ -23,24 +24,87 @@ const Container = () => {
         })
       }
     }
-    console.log(">>>BoardGenerated", tempBoard);
+    // console.log(">>>BoardGenerated", tempBoard);
     setBoard(tempBoard)
   }
 
-  return (
-    <div>
-      <button type="button">Add +</button>
+  function addPiece(){
+    let action = false;
+    let tempB = board.map(r=>{
+      r.map(c=>{
+        if(!c.piece && !action){
+          c.piece = true;
+          action = true;
+        }
+      })
+      return r;
+    })
+    setBoard(tempB)
+  }
 
+  function removePiece(){
+    let action = false;
+    let tempB = board.map(r=>{
+      r.map(c=>{
+        if(c.piece && !action){
+          c.piece = null;
+          action = true;
+        }
+      })
+      return r;
+    })
+    setBoard(tempB)
+  }
+
+  const handleDragStart = (e,cell) => {
+    // console.log(">>>>handleDragStart",e);
+    setDragging(cell);
+  };
+
+  const handleDrop = (e,cell) => {
+    e.preventDefault();
+    console.log(">>>>handleDrop",e,cell);
+    movePiece(dragging,cell)
+    setDragging(null)
+  };
+
+  const movePiece=(from,to)=>{
+    // console.log(">>>>MOVE",from,to);
+    const mBoard = board;
+    let {piece,x,y} = from;
+    if(piece){
+      mBoard[x][y].piece = null
+      mBoard[to.x][to.y].piece = piece
+      setBoard(mBoard)
+    }
+  }
+
+  const handleDragOver = (event) => {
+    event.preventDefault(); // This is required to allow dropping
+  };
+
+
+  return (
+    <div className='boardContainer'>
+      <button type="button" onClick={()=>addPiece()}>Add +</button>
+      <button type="button" className='removeBtn' onClick={()=>removePiece()}>Remove</button>
       {
         board?.length ? <div className='board'>
-
           {
             board.map((row, i) => (
               <div className='boardRow' key={i}>
                 {
                   row.map((cell, j) => (
-                    <div className='boardCell' key={cell.id}>
-                      Cell
+                    <div className='boardCell' key={cell.id} 
+                      onDragOver={handleDragOver}
+                      onDrop={e=>handleDrop(e, cell)}
+                    >
+                      {cell?.piece ? 
+                        <div 
+                          className='piece' 
+                          draggable onDragStart={(e) => handleDragStart(e,cell)}
+                        ></div>
+                        :null}
                     </div>
                   ))
                 }
